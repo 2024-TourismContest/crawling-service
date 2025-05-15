db8794bae044d237512cd7d197072c755f163b72
Minseok Choi <gd10080008@gmail.com>
2025-05-15 11:05:52 +0900

    Update Domain

---
+++ src/main/java/yaguhang/crawling/baseball/domain/Baseball.java
@@ -0,0 +1,44 @@
+package yaguhang.crawling.baseball.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(of = {"home", "away", "time"})
+@Table(name = "Baseball",
+        uniqueConstraints= {
+                @UniqueConstraint(
+                        name = "home, away, time, location 조합은 단일이어야함",
+                        columnNames = {
+                                "home",
+                                "away",
+                                "time",
+                                "location"
+                        }
+                )
+        }
+)
+public class Baseball {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime time;
+    private String weekDay;
+    private String home;
+    private String away;
+    private String location;
+    private String status;
+    private String homePitcher;
+    private String awayPitcher;
+    private int homeScore;
+    private int awayScore;
+}
